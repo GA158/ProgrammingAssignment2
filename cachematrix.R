1f7e595f3c3208f9428d26e34d25c584a354f390
GA158 <72348996+GA158@users.noreply.github.com>
2020-10-04 20:43:39 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,9 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
-}
-
++m <- NULL
++set <- function(y) {
++x <<- y
++m <<- NULL
++}
++get <- function() x
++setinverse <- function(mean) m <<- inverse 
++getinverse <- function() m
++list(set = set, get = get,
++setinverse = setinverse,
++getinverse = getinverse)
++
++}
++cacheSolve <- function(x, ...) {
++m <- x$getinverse()
++if(!is.null(m)) {
++message("getting cached data")
++return(m)
++}
++data <- x$get()
++m <- solve(data, ...)
++x$setinverse(m)
++m
++}
 
 ## Write a short comment describing this function
 
